b5c00c704bac10b64a8c903c6397156faaa57c84
shawonNirob <shawonnirob16@gmail.com>
2022-11-11 21:51:35 +0600

    Java OOPs

--- src/Constructor/defaultConstructor.java
+++ src/Constructor/defaultConstructor.java
@@ -1,9 +1,17 @@
 package Constructor;
 
 public class defaultConstructor {
+    int number;
+    int number2;
+    public defaultConstructor (){
+        number = 10;
+        number2 = 20;
+    }
     public void print(int n){
         for(int i=0; i<n; i++){
             System.out.print(i+ " ");
+            System.out.print(number + number2);
+            System.out.println();
         }
     }
     public static void main(String[] args) {
