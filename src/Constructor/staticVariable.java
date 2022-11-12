0845148c499e7b839b7a2993c3a5c2f0c87f43d6
shawonNirob <shawonnirob16@gmail.com>
2022-11-13 02:59:38 +0600

    Java OOPs

---
+++ src/Constructor/staticVariable.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+public class staticVariable {
+    int a;
+    static int b = 10;
+
+    static {
+        System.out.println(b);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
